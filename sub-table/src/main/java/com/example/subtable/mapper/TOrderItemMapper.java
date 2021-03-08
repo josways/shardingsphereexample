68f7434c341024af09b5a8fca8810be37b76219c
Josway <jxh98@foxmail.com>
2021-03-08 18:09:03 +0800

    add annotation

--- sub-table/src/main/java/com/example/subtable/mapper/TOrderItemMapper.java
+++ sub-table/src/main/java/com/example/subtable/mapper/TOrderItemMapper.java
@@ -3,7 +3,9 @@ package com.example.subtable.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.subtable.domain.TOrderItem;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TOrderItemMapper extends BaseMapper<TOrderItem> {
 
     @Update("truncate table t_order_item")
